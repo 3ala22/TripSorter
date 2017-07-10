4534a27ab2af5b0e76727e79c24ed3f309cb50b5
alahawash <alaa@snappcard.com>
2017-07-10 23:24:15 +0400

    Create boarding card classes

---
+++ src/TripSorter/TripSorter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TripSorter;
+
+class TripSorter
+{
+    /**
+     * Sort a set of unordered boarding passes.
+     *
+     * @param array $boardingCards
+     * @return array
+     */
+    public function sort(array $boardingCards)
+    {
+        $sortedCards = [];
+
+        return $sortedCards;
+    }
+}
---
+++ test/TripSorter/TripSorterTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: alahawash
+ * Date: 7/10/17
+ * Time: 11:18 PM
+ */
+class TripSorterTest
+{
+
+}
